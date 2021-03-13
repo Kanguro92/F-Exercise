0bd5ef1877a5a86bc8ab1b22d2ba2664fe7c53b5
Kanguro92 <33893238+Kanguro92@users.noreply.github.com>
2021-03-13 22:50:51 +0100

    Update es2.fsx

--- es2.fsx
+++ es2.fsx
@@ -1,91 +1,48 @@
-(*
-Esercizio 1: esponenziale
-^^^^^^^^^^^^^^^^^^^^^^^
-
-Definire una funzione ricorsiva
+(* Definire una funzione ricorsiva
 
    exp : float -> int -> float
 
 che calcola l'esponenziale:
 
   exp b n = b ^n   
-
-Si *assume* n>= 0. 
+ 
 Usiamo la seguente definizione ricorsiva (induzione su n)
 
   b^n   =  1                se n = 0    [CASO BASE}
 
   b^n  =   b * b^(n-1)      se n > 0    [PASSO INDUTTIVO] 
-
 *)
 
-// exp : b:float -> n:int -> float
 // si *assume*  n>= 0
 let rec exp b n =
     match n with
     | 0 -> 1.
     | _ -> b * exp b (n-1)
 
-(*
-
-Esercizio 2
-^^^^^^^^^^^
-
-i) Definire una funzione ricorsiva
+(* i) Definire una funzione ricorsiva
 
 make_str : int -> string
 
-che, dato un intero n>=0, costruisce la stringa "0 1 2 ... n"
+che, dato un intero n>=0, costruisce la stringa "0 1 2 ... n" *)
 
-Esempio:
-
-make_str 20 
-
-deve costruire la stinga
-
- "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
-
-Per concatenare stringhe, usare operatore +.
-Per convertire da intero a stringa, usare la funzione
-
-   string : int ->  string
-
-*)
-
-// i)
-// make_str : n:int -> string
-// si *assume*  n>= 0
 let rec make_str n =
     match n with
     | 0 -> string 0
     | _ -> make_str (n - 1) + " " + string n;;
+    
+// Esempio:
+make_str 20 
+// "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 
-(*
-
-ii) Definire la funzione ricorsiva
+(* Definire la funzione ricorsiva
 
   make_sum_str  int -> int * string
 
-tale che 
-
-  make_sum_str n
-
-calcola la coppia (sum,str) dove:
-
+Suggerimento: calcola la coppia (sum,str) dove:
 - sum e' l'intero corrispondente alla somma  0 + 1 + 2 + ... + n
 - str e' stringa "0 1 2 ... n"  (come nel punto precedente)
+Va fatta una *unica* chiamata ricorsiva *)
 
-Va fatta una *unica* chiamata ricorsiva  
-
-Esempio:
-
-mk_sum_str 5 
- val it : int * string = (15, "0 1 2 3 4 5")
-
-*)
-
-// ii)
-// mk_sum_str : n:int -> int * string
 // si *assume*  n>= 0
 let rec mk_sum_str n = 
     match n with
@@ -94,12 +51,12 @@ let rec mk_sum_str n =
         let (sum, str) = mk_sum_str (n-1)
         (sum + n, str + " " + string n)
 
+// Esempio:
 mk_sum_str 5 
 // val it : int * string = (15, "0 1 2 3 4 5")
 
-(*
 
-iii) Definire  una funzione
+(* Definire  una funzione
 
   somma_n : int -> string
 
@@ -111,23 +68,11 @@ stampa una stringa della forma
 
   "0 + 1 + ... + n = k"
 
-con k il valore della somma 0+1+ ... +k
+con k il valore della somma
 
-Suggerimento
-^^^^^^^^^^^^
-
-Definire una funzione ausiliaria
-
- make_sum_str1  int -> int * string
-
+Suggerimento: Definire una funzione ausiliaria: make_sum_str1  int -> int * string
 analoga alla funzione make_sum_str del punto ii) in cui pero' la stringa ha 
-il formato
-
- "0 + 1 + ... + n"
-
-La funzione ausiliaria puo' anche essere definita internamente alla funzione somma_n
-
-*)
+il formato "0 + 1 + ... + n" *)
 
 let somma_n n =
     let rec make_sum_str1 n =
@@ -151,10 +96,10 @@ let rec make_sum_str1 n =
 let somma_n n = 
     let (sum, str) = make_sum_str1 n
     str + " = " + string sum
-    
+
+// Esempio:
 let str1 = somma_n 5
 // "0 + 1 + 2 + 3 + 4 + 5 = 15"
-
 let str2 = somma_n 10
 // "0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55"
 
